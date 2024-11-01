407ae8419219bca9f889d65611a0a94b9212adea
FenosoaRandrianjatovo <frandrianjatovo@aimsammi.org>
2024-10-31 20:50:20 -0400

    Blast done

--- exercice2.py
+++ exercice2.py
@@ -5,7 +5,7 @@ import subprocess
 def create_output_directory(output_dir):
     """
     Crée le répertoire de sortie s'il n'existe pas.
-    :param output_dir: Chemin vers le répertoire de sortie
+    :param output_dir: Chemin vers le répertoire de sortie ou d'entrer
     """
     os.makedirs(output_dir, exist_ok=True)
     print(f"Le répertoire de sortie '{output_dir}' a été créé avec succès ou existe déjà. \n")
@@ -24,25 +24,32 @@ def process_fasta_file(input_file, output_dir, done):
     """
     
 
-    counter = 1 #this will help us to handle the existing ID for the sequence in faste file
+    counter = 1 # To avoid overwriting the existing file. 
+    # So This counter = 1 will help us to handle the existing ID for the sequence in faste file,
+    # by adding some index at the end of file name, like t7, t7_1
     # print(done)
     
     if not done: # If the exercice2 part 1 is not done
 
         with open(input_file, "r") as my_file_input:
             for line in my_file_input:
-                line = line.strip()  # Supprime les caractères de fin de ligne
+                line = line.strip()  # Delete \n
                 
-                # Vérifie si la ligne est un identifiant de séquence
+                # To check if the line start with >, ie: ID for the sequence
                 if line.startswith(">"):
-                    # Supprime le caractère ">" et nettoie le nom de fichier
+                    
+                    # Delete the char > and clean the name of the file by removing the \n
                     char = line[1:].strip()
-                    char = "".join(filter(str.isalnum, char))  # Conserve uniquement les caractères alphanumériques
 
-                    # Définit le nom et le chemin du fichier de sortie
+                    # Only keep the alphanumeric characters
+                    char = "".join(filter(str.isalnum, char)) 
+
+
+                    # Define the name and the path for the output file
                     filename = os.path.join(output_dir, f"{char}.fasta")
 
-                    # Ajoute un suffixe numérique tant que le fichier existe car t7 repete deux fois
+                    
+                    # To add a number for the existing ( file like the case for t7 )
                     while os.path.exists(filename):
                         filename = os.path.join(output_dir, f"{char}_{counter}.fasta")
                         counter += 1
@@ -61,7 +68,7 @@ def process_fasta_file(input_file, output_dir, done):
 
     else:
         print("The exercice 2 part 1 is aleardy done, we have created the all the fasta files \n \
-              Now we blast the files that we have already created \n")
+              Now we  will blast the files that we have already created \n")
 
         # Exercise 2b
         run_blast_for_fasta_files()
@@ -85,21 +92,24 @@ def run_blast_for_fasta_files(db="BLAST_db/H4476DB",
 
     # To loop through each FASTa flies inside the request folder
     for fasta_file in os.listdir(input_folder):
+        # To proccess only the fasta file inside the folder
         if fasta_file.endswith(".fasta"):
-            # Obtenir le nom de base du fichier (sans l'extension)
+
+            # To get the base name of the file wiyhout the extension
             base_name = os.path.splitext(fasta_file)[0]
 
-            # Chemin complet du fichier de sortie pour ce fichier de requête
+
+            # The full path of the output  files 
             output_file = os.path.join(output_folder_for_Blast, f"{base_name}_blast_resultats.txt")
 
 
             # The full  path for the request folder
             fasta_path = os.path.join(input_folder, fasta_file)
 
-
             # To run the Blast command line for each request
             subprocess.run(["blastn", "-query", fasta_path, "-db", db, "-out", output_file])
 
+            # We are done for a single file and we print
             print(f"BLAST terminé pour {fasta_path}. Résultats enregistrés dans {output_file}.")
 
 
